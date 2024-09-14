27457aec9d0a9869383c39b75aa87e2b3a098193
djawad <ha191952@ens.uqam.ca>
2024-09-14 00:27:44 -0400

    Implementation de la sauvegarde et lecture de l'authentificateur dans le localStorage via le service d'authentification

--- frontend/src/app/login/services/authentication.service.ts
+++ frontend/src/app/login/services/authentication.service.ts
@@ -12,14 +12,30 @@ export class AuthenticationService {
   constructor() {
     this.username.set(localStorage.getItem(AuthenticationService.KEY));
   }
+  
+  
+  sauvegarderAuthentificateur(userCredentials: UserCredentials){
+    localStorage.setItem(userCredentials.username,userCredentials.password);
+  }
+
+  lireAuthentificateur(userCredentials: UserCredentials){
+    return localStorage.getItem(userCredentials.username);
+  }
+
 
   login(userCredentials: UserCredentials) {
+    console.log('la requete d authentification est transmise au service');
+    this.sauvegarderAuthentificateur(userCredentials);
+    const value = this.lireAuthentificateur(userCredentials);
+    console.log(value);
     // À faire
   }
 
   logout() {
     // À faire
   }
+
+  
 
   getUsername(): Signal<string | null> {
     return this.username;
