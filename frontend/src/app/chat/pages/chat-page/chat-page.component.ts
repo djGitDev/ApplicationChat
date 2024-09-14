2e91345a42f81a11721e55ee7eb415a9859b221c
djawad <ha191952@ens.uqam.ca>
2024-09-14 19:48:56 -0400

    Reduction des responsabilites pour le composant page-chat intelligent

--- frontend/src/app/chat/pages/chat-page/chat-page.component.ts
+++ frontend/src/app/chat/pages/chat-page/chat-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal, effect } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
+import { ComposantAffichageComponent } from '../../composantsChat/composant-affichage/composant-affichage.component';
+import { ComposantPublicationMsgComponent} from '../../composantsChat/composant-publication-msg/composant-publication-msg.component'
 import { AuthenticationService } from 'src/app/login/services/authentication.service';
-import { Message } from '../../model/message.model';
 import { MessagesService } from '../../services/messages.service';
 
 
@@ -11,56 +12,36 @@ import { MessagesService } from '../../services/messages.service';
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule, DatePipe],
+  imports: [ReactiveFormsModule, DatePipe, ComposantAffichageComponent, ComposantPublicationMsgComponent],
 })
-export class ChatPageComponent {
+export class ChatPageComponent   {
+
+
 
   username = this.authenticationService.getUsername();
   messages = this.messagesService.getMessages();
-
-
-  messageForm = this.fb.group({
-    msg: '',
-  });
-
+  
   constructor(
-    private fb: FormBuilder,
     private messagesService: MessagesService,
     private authenticationService: AuthenticationService
     
   ) {}
 
-  onPublishMessage() {
-    if (
-      this.username() &&
-      this.messageForm.valid &&
-      this.messageForm.value.msg
-    ) {
+  onPublishMessage(entree: string) {
+    if (this.username()) {
       this.messagesService.postMessage({
-        text: this.messageForm.value.msg,
+        text: entree,
         username: this.username()!,
         timestamp: Date.now(),
       });
     }
-    this.messageForm.reset();
-  }
-
-  /** Afficher la date seulement si la date du message précédent est différente du message courant. */
-  showDateHeader(messages: Message[] | null, i: number) {
-    if (messages != null) {
-      if (i === 0) {
-        return true;
-      } else {
-        const prev = new Date(messages[i - 1].timestamp).setHours(0, 0, 0, 0);
-        const curr = new Date(messages[i].timestamp).setHours(0, 0, 0, 0);
-        return prev != curr;
-      }
-    }
-    return false;
   }
 
   onLogout() {
     this.authenticationService.logout();
-    // À faire
   }
+
+
+  
+
 }
